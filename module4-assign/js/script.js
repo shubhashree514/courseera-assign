7c4b69571e911c35f18586062ac4f109561c8c70
shubhashree514 <shubhashree.s.45@gmail.com>
2017-03-25 15:06:02 +0530

    Add files via upload

---
+++ module4-assign/js/script.js
@@ -0,0 +1,14 @@
+﻿
+(function () {
+
+var names = ["Shubha", "shree","Sandeshsharma", "Sheshadri", "Rathna", "Divya", "Naveen", "Jayalakshmi", "Jayanth", "pushpa" , "Satheesh"];
+
+for(var i=0;i < names.length;i++){
+if(names[i].charAt(0) == "J" || names[i].charAt(0) == "j"){
+	console.log("Hello "+names[i]);
+}else{
+	console.log("Good Bye "+names[i]);
+}
+}
+
+})();
